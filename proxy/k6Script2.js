c6d0824becacf994f7da39b43c71ab48ee1a1944
Kevin Gutierrez <kevin.w.gutierrez@gmail.com>
2020-02-28 19:31:30 -0800

    loader.io

--- proxy/k6Script2.js
+++ proxy/k6Script2.js
@@ -2,9 +2,9 @@ import { check, sleep } from "k6";
 import http from "k6/http";
 
 export let options = {
-  vus: 100,
-  rps: 100,
-  duration: "5m0s"
+  vus: 800,
+  rps: 800,
+  duration: "3m0s"
 };
 
 // add ip
